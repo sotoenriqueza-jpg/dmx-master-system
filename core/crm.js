145c2efd7ae59f16034723039653af5a4727700e
Alexandro Soto <sotoenriqueza@gmail.com>
2026-02-25 14:45:15 -0600

    Add procesarCRM function to update CRM sheet

--- core/crm.js
+++ core/crm.js
@@ -1,1 +1,19 @@
+// =============================
+// MÓDULO CRM
+// =============================
 
+function procesarCRM(p) {
+
+  const sheet = SpreadsheetApp.getActiveSpreadsheet()
+                .getSheetByName("CLIENTES");
+
+  sheet.appendRow([
+    new Date(),
+    p.idCte,
+    p.nombre,
+    p.telefono,
+    p.tipoCte
+  ]);
+
+  return "CRM actualizado";
+}
