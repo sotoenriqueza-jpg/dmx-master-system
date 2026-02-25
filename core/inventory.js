3cf701679530fde57739bfbec77932cf14a00565
Alexandro Soto <sotoenriqueza@gmail.com>
2026-02-25 14:45:50 -0600

    Add inventory processing function

--- core/inventory.js
+++ core/inventory.js
@@ -1,1 +1,21 @@
+// =============================
+// MÓDULO INVENTARIO
+// =============================
 
+function procesarInventario(p) {
+
+  const sheet = SpreadsheetApp.getActiveSpreadsheet()
+                .getSheetByName("INVENTARIO");
+
+  const data = sheet.getDataRange().getValues();
+
+  for (let i = 1; i < data.length; i++) {
+    if (data[i][0] === p.producto) {
+      const stockActual = data[i][1];
+      sheet.getRange(i + 1, 2).setValue(stockActual - p.cantidad);
+      return "Stock actualizado";
+    }
+  }
+
+  return "Producto no encontrado";
+}
