5a15e679d766abcaa617361eb286ce8b05b690ef
Alexandro Soto <sotoenriqueza@gmail.com>
2026-02-25 14:51:54 -0600

    Implement report registration function
    Added a function to register reports in a spreadsheet.

--- core/reports.js
+++ core/reports.js
@@ -1,1 +1,21 @@
 
+// =============================
+// MÓDULO REPORTES
+// =============================
+
+function registrarReporte(p) {
+
+  const sheet = SpreadsheetApp.getActiveSpreadsheet()
+                .getSheetByName("CONSOLIDADO");
+
+  sheet.appendRow([
+    new Date(),
+    p.vendedor,
+    p.producto,
+    p.cantidad,
+    p.precio,
+    p.envio
+  ]);
+
+  return "Reporte registrado";
+}
